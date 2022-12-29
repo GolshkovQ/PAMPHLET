23c26e5eb40cb5bf59e1ff7673fe1a2e31470856
Chen Qi <39559610+GolshkovQ@users.noreply.github.com>
2022-12-29 16:42:51 +0800

    Update OnlineResources.py

--- PAMPHLET/OnlineResources.py
+++ PAMPHLET/OnlineResources.py
@@ -12,6 +12,8 @@ import sys
 import os
 import requests
 import time
+import func_timeout
+from func_timeout import func_set_timeout
 from urllib.parse import quote
 from pyfaidx import Fasta
 
@@ -65,18 +67,28 @@ def run_online_blastp(infile,outfile):
         os.system("rm "+outfile+".temp")
         return True
 
-def run_spacer_blast(infile,outfile,indb):
+def run_spacer_blast(infile,outfile,indb,blastmode):
     ### This online blastn function, which is based on NCBI BLAST, is used to predict the putative protospacer sources. 
     ### Search against viruses database, which taxid = 10239
 
     UpLoadQuery = quote(open(infile,'r').read())
     if indb == 'phage':
-        arguments = "CMD=Put&PROGRAM=blastn&MEGABLAST=on&DATABASE=nt&ENTREZ_QUERY=txid10239[ORGN]&QUERY=" + UpLoadQuery
+        if blastmode == 'relax':
+            arguments = "CMD=Put&PROGRAM=blastn&MEGABLAST=on&NUCL_REWARD=1&WORD_SIZE=16&GAPCOSTS=5 2&NUCL_PENALTY=-1&DATABASE=nt&ENTREZ_QUERY=txid10239[ORGN]&QUERY=" + UpLoadQuery
+        else:
+            arguments = "CMD=Put&PROGRAM=blastn&MEGABLAST=on&DATABASE=nt&ENTREZ_QUERY=txid10239[ORGN]&QUERY=" + UpLoadQuery
     else:
-        arguments = "CMD=Put&PROGRAM=blastn&MEGABLAST=on&DATABASE=nt&ENTREZ_QUERY=NOT%20txid2759[ORGN]&QUERY=" + UpLoadQuery
+        if blastmode == 'relax':
+            arguments = "CMD=Put&PROGRAM=blastn&MEGABLAST=on&NUCL_REWARD=1&WORD_SIZE=16&GAPCOSTS=5 2&NUCL_PENALTY=-1&DATABASE=nt&ENTREZ_QUERY=NOT%20txid2759[ORGN]&QUERY=" + UpLoadQuery
+        else:
+            arguments = "CMD=Put&PROGRAM=blastn&MEGABLAST=on&DATABASE=nt&ENTREZ_QUERY=NOT%20txid2759[ORGN]&QUERY=" + UpLoadQuery
 
     r = requests.put("https://blast.ncbi.nlm.nih.gov/Blast.cgi?"+arguments)
     RID = r.text.split("RID = ")[1].split()[0]
+    while RID == "":
+        print("RID extraction failed, re-searching...")
+        time.sleep(2)
+        RID = r.text.split("RID = ")[1].split()[0]
 
     print("SPACER BLAST PROGRAM RID: "+RID)
 
@@ -86,6 +98,7 @@ def run_spacer_blast(infile,outfile,indb):
     while Status != "READY":
         time.sleep(30)
         rStatus = requests.get("https://blast.ncbi.nlm.nih.gov/Blast.cgi?CMD=Get&FORMAT_OBJECT=SearchInfo&RID="+RID)
+        Status = rStatus.text.split("Status=")[1].split()[0]
         Status = rStatus.text.split("Status=")[1].split()[0]
         print("Refresh in 30s, status: "+Status)
 
@@ -109,7 +122,31 @@ def run_spacer_blast(infile,outfile,indb):
     os.system("rm "+outfile+".temp")
     return True
 
-def get_seq_dump_file(insig,outdump):
+### Set timeout for get_session_post_text function, time out limit is 10s
+@func_set_timeout(10)
+def get_session_post_text(inid):
+    urls = "https://www.ncbi.nlm.nih.gov/ipg/"+inid
+    session = requests.Session()
+    r5 = session.get(urls)
+    url = "https://www.ncbi.nlm.nih.gov/sviewer/ipg/ipg.cgi?"
+    post_data = {'query':'omitHeader%3Dfalse%26wt%3Dxml%26indent%3Dtrue%26rows%3D50%26start%3D0%26q%3D'+ inid +'%26sort%3Dpriority_i%2520asc'}
+    post_data_1 = {'db':'ipg','solr':'1','id':inid}
+    session.post(url,data= post_data_1).text
+    r6 = session.post(url,data=post_data).text
+    session.cookies.clear()
+    return r6
+
+@func_set_timeout(20)
+def get_protein_sequences(inurl):
+    proteintext = requests.get(inurl).text
+    return proteintext
+
+@func_set_timeout(20)
+def get_genome_fasta(inurl):
+    genometext = requests.get(inurl).text
+    return genometext
+
+def get_seq_dump_file(insig,outdump,inmaxsize):
     infodict = {}
     tempdict = {}
     duplicateBox = []
@@ -119,11 +156,18 @@ def get_seq_dump_file(insig,outdump):
     proteinpage = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=protein&id="
     with open(insig,'r') as fa, open(outdump,'w') as fb:
         for faline in fa.readlines():
-            if len(duplicateBox) == 10:
+            if len(duplicateBox) == inmaxsize:
                 break
             sbjctID = faline.split("\t")[1]
             protein2fasta = proteinpage + sbjctID + "&rettype=fasta"
-            proteinfasta = requests.get(protein2fasta).text
+            retry_counter = 0
+            while retry_counter < 5:
+                try:
+                    proteinfasta = get_protein_sequences(protein2fasta)
+                    break
+                except:
+                    retry_counter += 1
+                    print("Retry in: "+sbjctID+", retry time: "+str(retry_counter))
             try:
                 proteinsequence = proteinfasta.split("\n")[1]
             except:
@@ -132,15 +176,15 @@ def get_seq_dump_file(insig,outdump):
                 continue
             if not proteinsequence in duplicateBox:
                 URLCorrectFlag = True
-                url5 = "https://www.ncbi.nlm.nih.gov/ipg/"+sbjctID
-                session = requests.Session()
-                r5 = session.get(url5)
-                url = "https://www.ncbi.nlm.nih.gov/sviewer/ipg/ipg.cgi?"
-                post_data = {'query':'omitHeader%3Dfalse%26wt%3Dxml%26indent%3Dtrue%26rows%3D50%26start%3D0%26q%3D'+ sbjctID +'%26sort%3Dpriority_i%2520asc'}
-                post_data_1 = {'db':'ipg','solr':'1','id':sbjctID}
-                session.post(url,data= post_data_1).text
-                r6 = session.post(url,data=post_data).text
-                session.cookies.clear()
+                retry_counter = 0
+                ### Set timeout for get_session_post_text function, time out limit is 10s
+                while retry_counter < 5:
+                    try:
+                        r6 = get_session_post_text(sbjctID)
+                        break
+                    except:
+                        retry_counter += 1
+                        print("Retry in: "+sbjctID+", retry time: "+str(retry_counter))
                 genomerefheader = "<str name=\"cds_accver_s\">"
                 genome_tax_header = "<str name=\"org_s\">"
                 orfstartheader = "<long name=\"cds_start_l\">"
@@ -160,10 +204,19 @@ def get_seq_dump_file(insig,outdump):
                         genomedupbox.append(refid)
                         tempdict[refid] = sbjctID
                         gi2fasta = genomepage+refid+"&rettype=fasta"
-                        genomefasta = requests.get(gi2fasta)
-                        if ">" in genomefasta.text and not "Error" in genomefasta.text and not "error" in genomefasta.text:
-                            fb.write(genomefasta.text[:-1])
-                            fb.flush()
+                        ### retry for 5 times
+                        retry_counter = 0
+                        while retry_counter < 5:
+                            try:
+                                genomefasta = get_genome_fasta(gi2fasta)
+                                if ">" in genomefasta and not "Error" in genomefasta and not "error" in genomefasta:
+                                    fb.write(genomefasta[:-1])
+                                    fb.flush()
+                                    break
+                            except:
+                                retry_counter += 1
+                                print("Time out in: "+sbjctID+", retry in "+str(retry_counter)+" time(s)...")
+                                continue
                 else:
                     print("Failed ID convert for: "+sbjctID)
         fa.close()
@@ -190,6 +243,11 @@ def get_seq_dump_file(insig,outdump):
 
     return infodict
 
+@func_set_timeout(10)
+def get_genome_request_text(inurl):
+    r = requests.get(inurl)
+    return r.text
+
 def get_genome_seqdump_files(inhit,outfile):
     seqdumplimit = 500
     seqdumpcounter = 0
@@ -205,11 +263,21 @@ def get_genome_seqdump_files(inhit,outfile):
                     break
                 duplicatebox.append(sbjctid)
                 gi2fasta = genomepage + sbjctid + "&rettype=fasta"
-                genomefasta = requests.get(gi2fasta)
-                if ">" in genomefasta.text and not "Error" in genomefasta.text and not "error" in genomefasta.text:
-                    seqdumpcounter += 1
-                    fb.write(genomefasta.text[:-1])
-                    fb.flush()
+                retry_counter = 0
+                ### if false, retry 5 times
+                while retry_counter < 5:
+                    try:
+                        genomefasta = get_genome_request_text(gi2fasta)
+                        if ">" in genomefasta and not "Error" in genomefasta and not "error" in genomefasta:
+                            seqdumpcounter += 1
+                            fb.write(genomefasta[:-1])
+                            break
+                        else:
+                            continue
+                    except:
+                        retry_counter += 1
+                        print("Time out in: "+sbjctid+", retry for "+str(retry_counter)+" time(s)...")
+                        continue
             else:
                 continue
     fa.close()
