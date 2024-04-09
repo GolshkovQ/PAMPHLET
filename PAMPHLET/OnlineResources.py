1cf61f4237e85ac6a77b6a59eb7c8262598c4753
Chen Qi <39559610+GolshkovQ@users.noreply.github.com>
2024-04-09 17:05:37 +0800

    Update OnlineResources.py

--- PAMPHLET/OnlineResources.py
+++ PAMPHLET/OnlineResources.py
@@ -3,7 +3,7 @@
 '''
 PAMPHLET - PAM Prediction HomoLogous Enhancement Toolkit
 Author: Chen Qi, Baitao Li, Lei Huang
-Beijing Normal University-Hong Kong Baptist University United Internation College, FST-DSDS, Zhuhai, China
+Beijing Normal University-Hong Kong Baptist University United International College, FST-DSDS, Zhuhai, China
 University of Chinese Academy of Sciences, College of Life Sciences, Beijing, China
 BGI Research, Shenzhen, China
 Email: qichen@genomics.cn; libaitao@genomics.cn; huanglei@genomics.cn
